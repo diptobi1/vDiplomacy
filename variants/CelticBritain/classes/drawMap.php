0e919c3791352cac32fc277dcbf404d2e9eb8d7f
Oliver Auth <Oliver.Auth@rhoen.de>
2017-10-29 18:32:43 +0100

    Improvements for the CelticBritain variant. And an emergancy fix for the special-coast-convoy-code. At the moment it just disables the server-side check. Will improve this later.

--- variants/CelticBritain/classes/drawMap.php
+++ variants/CelticBritain/classes/drawMap.php
@@ -14,15 +14,18 @@ class CelticBritainVariant_drawMap extends drawMap {
 
 	protected $countryColors = array(
 		0 => array(211, 249, 188),
-		1 => array(255, 126,   0),
-		2 => array(111,  49, 152),
-		3 => array(  0, 183, 239),
-		4 => array( 67,  72, 224),
-		5 => array( 34, 117,  76),
-		6 => array(168, 230,  29),
-		7 => array(254, 162, 176),
-		8 => array(153,   0,  48),
-			);
+		1 => array(255, 126,   0), // Brigantes
+		2 => array(111,  49, 152), // Iceni
+		3 => array(  0, 183, 239), // Caledonii
+		4 => array( 67,  72, 224), // Picts
+		5 => array( 34, 117,  76), // Cornovii
+		6 => array(168, 230,  29), // Ivernia
+		7 => array(254, 162, 176), // Voluntii
+		8 => array(153,   0,  48), // Durotriges
+	);
+	
+	// No need to set the transparency for our custom icons and mapnames.
+	protected function setTransparancy(array $image, array $color=array(255,255,255)) {}
 
 	protected function resources() {
 		if( $this->smallmap )
@@ -46,7 +49,36 @@ class CelticBritainVariant_drawMap extends drawMap {
 			);
 		}
 	}
+	
+	// Draw the flags behind the units for a better readability
+	public function countryFlag($terrName, $countryID)
+	{
+		$flagBlackback = $this->color(array(0, 0, 0));
 
+		$flagColor = $this->color($this->countryColors[$countryID]);
+
+		list($x, $y) = $this->territoryPositions[$terrName];
+
+		$coordinates = array(
+			'top-left' => array( 
+							'x'=>$x-intval($this->fleet['width']/2+2),
+							'y'=>$y-intval($this->fleet['height']/2+2)-1
+							),
+			'bottom-right' => array(
+							'x'=>$x+intval($this->fleet['width']/2+2)-1,
+							'y'=>$y+intval($this->fleet['height']/2+2)+1
+							)
+			);
+
+		imagefilledrectangle($this->map['image'],
+			$coordinates['top-left']['x'], $coordinates['top-left']['y'],
+			$coordinates['bottom-right']['x'], $coordinates['bottom-right']['y'],
+			$flagBlackback);
+		imagefilledrectangle($this->map['image'],
+			$coordinates['top-left']['x']+1, $coordinates['top-left']['y']+1,
+			$coordinates['bottom-right']['x']-1, $coordinates['bottom-right']['y']-1,
+			$flagColor);
+	}
 }
 
 ?>
--- variants/CelticBritain/resources/iconscorrect.js
+++ variants/CelticBritain/resources/iconscorrect.js
@@ -2,7 +2,7 @@ function IconsCorrect(VariantName) {
 	MyOrders.map(
 		function(OrderObj) {
 			newIcon=OrderObj.currentUnitIcon
-			OrderObj.unitIconArea.update('<img src="variants/'+VariantName+'/resources/'+newIcon.toLowerCase()+'.png" alt="'+newIcon+'" />');
+			OrderObj.unitIconArea.update('<img src="variants/'+VariantName+'/resources/Small'+newIcon.toLowerCase()+'.png" alt="'+newIcon+'" />');
 		},this
 	);
 }
