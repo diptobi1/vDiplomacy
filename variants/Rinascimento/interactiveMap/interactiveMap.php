f0ac1ed44b80ad7bc384ab414cff5a665f027051
Tobias Florin <tobi@casa-blu.de>
2018-03-10 00:00:59 +0100

    InteractiveMap for Rinascimento: Added Custom Build Icons

--- variants/Rinascimento/interactiveMap/interactiveMap.php
+++ variants/Rinascimento/interactiveMap/interactiveMap.php
@@ -16,6 +16,30 @@ class Nomove_IAmap extends IAmap{
         }
 }
 
-class RinascimentoVariant_IAmap extends Nomove_IAmap{}
+class CustomIcons_IAmap extends Nomove_IAmap{
+        public function __construct($variant, $mapName = 'IA_smallmap.png') {
+                parent::__construct($variant, $mapName);
+                
+                $this->buildButtonAutogeneration = true;
+        }
+        
+        protected function resources() {
+                return array(
+                        'army'=>l_s('variants/'.$this->Variant->name.'/resources/smallarmy.png'),
+                        'fleet'=>l_s('variants/'.$this->Variant->name.'/resources/smallfleet.png')
+                );
+        }
+        
+        protected function setTransparancies() {}
+        
+        protected function jsFooterScript() {
+                libHTML::$footerScript[] = "    interactiveMap.parameters.imgBuildArmy = 'interactiveMap/php/IAgetBuildIcon.php?unitType=Army&variantID=".$this->Variant->id."';
+                                        interactiveMap.parameters.imgBuildFleet = 'interactiveMap/php/IAgetBuildIcon.php?unitType=Fleet&variantID=".$this->Variant->id."';";
+        
+                parent::jsFooterScript();
+        }
+}
+
+class RinascimentoVariant_IAmap extends CustomIcons_IAmap{}
 
 ?>
