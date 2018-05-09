a81356f53e52c587ab0e8fa0c6c59a00eb988e0e
Tobias Florin <tobi@casa-blu.de>
2018-05-09 17:11:18 +0200

    Updated JS version numbering

--- global/definitions.php
+++ global/definitions.php
@@ -27,7 +27,7 @@ defined('IN_CODE') or die('This script can not be run by itself.');
 define("VERSION", 143);
 define("VDIPVERSION", 63);
 define("CSSVERSION", 12);
-define("JSVERSION", 15);
+define("JSVERSION", 16);
 
 // Some integer values which are named for clarity.
 
