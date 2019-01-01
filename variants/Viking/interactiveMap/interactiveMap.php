211bb8968d19fafce9104adcfdda7547ea981029
Tobias Florin <tobi@casa-blu.de>
2019-01-01 20:58:39 +0100

    InteractiveMap for Vikings - Custom build icons added

--- variants/Viking/interactiveMap/interactiveMap.php
+++ variants/Viking/interactiveMap/interactiveMap.php
@@ -10,8 +10,31 @@
  * @author tobi
  */
          
- 
-class CoastConvoyOrders_IAmap extends IAmap 
+class CustomBuildIcons_IAmap extends IAmap {
+        public function __construct($variant, $mapName = 'IA_smallmap.png') {
+                parent::__construct($variant, $mapName);
+                
+                $this->buildButtonAutogeneration = true;
+        }
+        
+        protected function resources() {
+                return array(
+                        'army'=>l_s('variants/'.$this->Variant->name.'/resources/smallarmy.png'),
+                        'fleet'=>l_s('variants/'.$this->Variant->name.'/resources/smallfleet.png')
+                );
+        }
+        
+        protected function setTransparancies() {}
+        
+        protected function jsFooterScript() {
+                libHTML::$footerScript[] = "    interactiveMap.parameters.imgBuildArmy = 'interactiveMap/php/IAgetBuildIcon.php?unitType=Army&variantID=".$this->Variant->id."';
+                                        interactiveMap.parameters.imgBuildFleet = 'interactiveMap/php/IAgetBuildIcon.php?unitType=Fleet&variantID=".$this->Variant->id."';";
+        
+                parent::jsFooterScript();
+        }
+} 
+
+class CoastConvoyOrders_IAmap extends CustomBuildIcons_IAmap 
 {
         protected function jsFooterScript() {
                 global $Variant;
