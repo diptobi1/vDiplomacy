83f78fac4a180e4205abc2eee80400af25258a23
Tobias Florin <tobi@casa-blu.de>
2019-01-01 19:51:01 +0100

    InteractiveMap for Viking

---
+++ variants/Viking/interactiveMap/interactiveMap.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IAmap
+ *
+ * @author tobi
+ */
+         
+ 
+class CoastConvoyOrders_IAmap extends IAmap 
+{
+        protected function jsFooterScript() {
+                global $Variant;
+                
+                parent::jsFooterScript();
+                
+                libHTML::$footerScript[] = 'loadCoastConvoyOrders(Array("'.implode($Variant->convoyCoasts, '","').'"))';
+        }
+}
+
+class VikingVariant_IAmap extends CoastConvoyOrders_IAmap {}
+
+?>
