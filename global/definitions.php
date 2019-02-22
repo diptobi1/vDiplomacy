675dccd0d21bc9b7e3ce6f10510335fb0777e95f
Tobias Florin <tobi@casa-blu.de>
2019-02-22 19:15:37 +0100

    Increased js-version after recent updates

--- global/definitions.php
+++ global/definitions.php
@@ -27,7 +27,7 @@ defined('IN_CODE') or die('This script can not be run by itself.');
 define("VERSION", 143);
 define("VDIPVERSION", 64);
 define("CSSVERSION", 15);
-define("JSVERSION", 20);
+define("JSVERSION", 21);
 
 // Some integer values which are named for clarity.
 
