dd80042d4f83be316bd2aed7bfceba9bbe6a658b
Oliver Auth <Oliver.Auth@rhoen.de>
2019-04-27 14:22:37 +0200

    Moved the startgame-tool in the dev-admin-panel.

--- dev/admin.php
+++ dev/admin.php
@@ -88,4 +88,55 @@ if ($User->id == 5)
 }
 print '<div class="hr"></div>';
 
+if ($User->id == 5)
+{
+	if ( isset($_REQUEST['gameID']) && isset($_REQUEST['fillGame']) )
+	{
+		$gameID=(int)$_REQUEST['gameID'];
+		require_once('gamemaster/game.php');
+
+		$Variant=libVariant::loadFromGameID($gameID);
+		libVariant::setGlobals($Variant);
+		$Game = $Variant->processGame($gameID);
+		
+		global $DB;
+		$dummy_add=0;
+		$Game->phaseMinutes = 60;
+
+		while (!($Game->needsProcess()))
+		{
+			$dummy_add++;
+			list($id)=$DB->sql_row("SELECT id FROM wD_Users WHERE username = 'dummy_".$dummy_add."'");
+			if (!($id))
+			{
+				$DB->sql_put("INSERT INTO wD_Users (username, email, points) VALUES ('dummy_".$dummy_add."', 'dummy_".$dummy_add."', 20000)");
+				list($id)=$DB->sql_row("SELECT id FROM wD_Users WHERE username = 'dummy_".$dummy_add."'");
+			}
+			processMember::create($id, $Game->minimumBet);
+		}
+		print "Filled the game with ".($dummy_add)." users. ";
+		print "Click <u><a href='board.php?gameID=".$Game->id."'>here</a></u> to open the game.";
+	}
+	else
+	{
+		print '<strong>Start Game:</strong><br>
+				Fill the remaining places with dummy players to start the game: 
+				<form style="display: inline" method="POST" name="start_game">
+					<select name=gameID>';
+				
+		$tabl = $DB->sql_tabl('SELECT id, variantID FROM wD_Games WHERE phase="Pre-Game"');
+		while(list($id, $variantID)=$DB->tabl_row($tabl))
+		{
+			$Variant=libVariant::loadFromVariantID($variantID);
+			$Game = $Variant->Game($id);
+			print '<option value="'.$Game->id.'"> ID:'.$Game->id.' - '.$Game->name.' ('.$Variant->name.')</option>';
+		}
+		print '</select>';
+		
+		print '<input type="hidden" name="fillGame" value="1" />
+				<input type="submit" class="form-submit" value="Fill game" />';
+	}
+	print '<div class="hr"></div>';
+}
+
 ?>
