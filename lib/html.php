92e4acf51ad430d2aa1f86b3da04e8e916f11801
Oliver Auth <Oliver.Auth@rhoen.de>
2019-06-11 20:11:29 +0200

    Adjusted menue entries.

--- lib/html.php
+++ lib/html.php
@@ -1001,11 +1001,14 @@ class libHTML
                         <div id="nav-drop">
 							<a href="gamelistings.php?gamelistType=New" title="Game listings; a searchable list of the games on this server">New Games</a>
 							<a href="gamelistings.php?gamelistType=Open%20Positions" title="Open positions dropped by other players, free to claim">Open Positions</a>
+							<a href="gamelistings.php?gamelistType=Active" title="All active games">Active games</a>
+							<a href="tournaments.php" title="Information about tournaments on vDiplomacy">Tournaments</a>
+							<div class="hr"></div>
 							<a href="gamecreate.php" title="Start up a new game">Create a New Game</a>
 './*							<a href="https://sites.google.com/view/webdipinfo/ghost-ratings" target=_blank title="Ghost Ratings (external site)">Ghost Ratings</a>
-*/'							<a href="tournaments.php" title="Information about tournaments on webDiplomacy">Tournaments</a>
+*/'							<div class="hr"></div>
 							<a href="tournamentInfo.php" title="Information about tournaments on webDiplomacy">Tournament info</a>
-							<a href="halloffame.php" title="Information about tournaments on webDiplomacy">Hall of Fame</a>
+							<a href="halloffame.php" title="Information about tournaments on vDiplomacy">Hall of Fame</a>
                         </div>
 
                     </div>
