5fe4d0e36db55cc8aced48e64217c65b491acade
Tobias Florin <tobi@casa-blu.de>
2019-08-04 19:45:44 +0200

    Fix for a minor bug of the interactive map resulting in deactivated save buttons

--- global/definitions.php
+++ global/definitions.php
@@ -48,7 +48,7 @@ define("JSDIR", 'javascript');
 define("CSSDIR", 'css');
 
 //Increment these versions whenever you update any js or css files for cachebusting
-define("JSVERSION",38);
+define("JSVERSION",39);
 define("CSSVERSION",35);
 
 if( !defined('FACEBOOK') )
